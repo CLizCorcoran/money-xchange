d0f02ed84cf83fb8d420ca4ed7151f439bb8bec8
Christie Corcoran <christiecorcoran30@gmail.com>
2021-04-14 15:55:53 -0400

    github issues

--- money_exchange/settings.py
+++ money_exchange/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+from secrets import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!n=qwgc!4!ce3g48)c-h-1chf*50bsf=!4it1!f@4pp%l98(!s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
