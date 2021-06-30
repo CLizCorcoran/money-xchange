cc76546bc59c8fde241f40c81612d5b23f7bd3c4
Christie Corcoran <christiecorcoran30@gmail.com>
2021-06-30 15:32:08 -0400

    More heroku misery

--- money_exchange/settings.py
+++ money_exchange/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['moneyxchange.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
